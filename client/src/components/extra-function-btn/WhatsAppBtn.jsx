17060d18a5181bdc3cdff4037ea553ded012a8e8
RaghavSaboo29 <raghavsaboo29@gmail.com>
2023-08-01 20:45:04 +0530

    updated

--- client/src/components/extra-function-btn/WhatsAppBtn.jsx
+++ client/src/components/extra-function-btn/WhatsAppBtn.jsx
@@ -5,7 +5,7 @@ const WhatsAppBtn = () => {
   return (
     <>
       <div className="wp-wrapper">
-        <a href="https://wa.me/+918989161972" target="_blank">
+        <a href="https://wa.me/+919007704578" target="_blank">
           <div className="wp-btn">
             <img src={WhatsAppImg} alt="whatsapp-icon" className="wp-img" />
           </div>
