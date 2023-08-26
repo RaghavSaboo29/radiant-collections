c09b9e5cf7c99f78f581f47c43421bd7b20f4e1e
RaghavSaboo29 <raghavsaboo29@gmail.com>
2023-08-26 09:47:21 +0530

    change

--- backend/server.js
+++ backend/server.js
@@ -15,8 +15,8 @@ import productRoutes from './routes/productRoutes.js'
 // dotenv.config()
 dotenv.config({ path: 'backend/.env' })
 
-const __filename= fileURLToPath(import.meta.url)
-const __dirname= path.dirname(__filename)
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 //database config
 connectDB()
@@ -32,7 +32,14 @@ cloudinary.config({
 const app = express()
 
 //middlewares
-app.use(cors())
+// app.use(cors())
+app.use(
+  cors({
+    origin: ['https://radiant-collections.vercel.app'],
+    methods: ['POST', 'GET'],
+    credentials: true,
+  })
+)
 app.use(express.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(fileUpload())
